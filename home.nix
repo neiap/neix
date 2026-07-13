3f8050364770ba997ad26fe4f4869db6010fa303
neiap <neiap@proton.me>
2026-07-14 01:21:07 +1000

    Firefox shortcuts, steam fix attempt

--- home.nix
+++ home.nix
@@ -23,6 +23,10 @@
       # SteamVR lighthouse driver never starts and trackers never show up.
       "use-steamvr-lh" = true;
     };
+    file.".steam/steam/steam_dev.cfg".text = ''
+      @nClientDownloadEnableHTTP2PlatformLinux 0
+      @fDownloadRateImprovementToAddAnotherConnection 1.0
+    '';
   };
 
   programs = {
@@ -38,7 +42,7 @@
       };
       extraEnv = ''
         $env.PROMPT_COMMAND = {||
-          $"(ansi purple_bold)($env.USER)(ansi reset)@(ansi cyan_bold)(sys host | get hostname)(ansi reset)"
+          $"(ansi purple_bold)($env.USER)(ansi reset)@(ansi cyan_bold)(sys host | get hostname)(ansi reset) (ansi green_bold)($env.PWD)(ansi reset)"
         }
       '';
     };
@@ -62,45 +66,61 @@
       enable = true;
       package = null; # already installed via programs.firefox.enable in configuration.nix
 
-      profiles.default.search = {
-        force = true;
-        engines = {
-          kagi = {
-            name = "Kagi";
-            urls = [ { template = "https://kagi.com/search?q={searchTerms}"; } ];
-            definedAliases = [ ":kg" ];
-          };
+      profiles.default = {
+        path = "6u0gf2tl.default";
 
-          nixos-options = {
-            name = "NixOS Options";
-            urls = [
-              {
-                template = "https://search.nixos.org/options?channel=unstable&from=0&size=50&sort=relevance&type=packages&query={searchTerms}";
-              }
-            ];
-            definedAliases = [ ":no" ];
-          };
+        search = {
+          force = true;
+          default = "kagi";
+          engines = {
+            kagi = {
+              name = "Kagi";
+              urls = [ { template = "https://kagi.com/search?q={searchTerms}"; } ];
+              definedAliases = [ ":kg" ];
+            };
 
-          nixpkgs = {
-            name = "Nix Packages";
-            urls = [
-              {
-                template = "https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query={searchTerms}";
-              }
-            ];
-            definedAliases = [ ":np" ];
-          };
+            nixos-options = {
+              name = "NixOS Options";
+              urls = [
+                {
+                  template = "https://search.nixos.org/options?channel=unstable&from=0&size=50&sort=relevance&type=packages&query={searchTerms}";
+                }
+              ];
+              definedAliases = [ ":no" ];
+            };
 
-          proton-db = {
-            name = "ProtonDB";
-            urls = [ { template = "https://www.protondb.com/search?q={searchTerms}"; } ];
-            definedAliases = [ ":db" ];
-          };
+            nixpkgs = {
+              name = "Nix Packages";
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query={searchTerms}";
+                }
+              ];
+              definedAliases = [ ":np" ];
+            };
 
-          youtube = {
-            name = "Youtube";
-            urls = [ { template = "https://www.youtube.com/results?search_query={searchTerms}"; } ];
-            definedAliases = [ ":yt" ];
+            noogle = {
+              name = "Noogle";
+              urls = [ { template = "https://noogle.dev/q?term={searchTerms}"; } ];
+              definedAliases = [ ":ng" ];
+            };
+
+            proton-db = {
+              name = "ProtonDB";
+              urls = [ { template = "https://www.protondb.com/search?q={searchTerms}"; } ];
+              definedAliases = [ ":pd" ];
+            };
+
+            youtube = {
+              name = "Youtube";
+              urls = [ { template = "https://www.youtube.com/results?search_query={searchTerms}"; } ];
+              definedAliases = [ ":yt" ];
+            };
+
+            "Wikipedia".metaData.hidden = true;
+            "bing".metaData.hidden = true;
+            "ddg".metaData.hidden = true;
+            "google".metaData.hidden = true;
           };
         };
       };
