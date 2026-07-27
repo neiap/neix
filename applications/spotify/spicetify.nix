ded93d0875c1436c18bba72f3d967b626fa7c6be
neiap <neiap@proton.me>
2026-07-27 16:00:03 +1000

    theme(spicetify): fix lyrics background, disable now playing view

--- applications/spotify/spicetify.nix
+++ applications/spotify/spicetify.nix
@@ -18,5 +18,24 @@ in
     ];
     theme = spicePkgs.themes.catppuccin;
     colorScheme = "mocha";
+
+    # override lyrics page background colour
+    enabledSnippets = [
+      ''
+        .lyrics-lyrics-background { display: none; }
+        .lyrics-lyrics-contentWrapper>*:not(.lyrics-lyricsContent-active, .lyrics-lyricsContent-highlight, .lyrics-lyricsContent-provider, .lyrics-lyricsContent-description, .lyrics-lyricsContent-unsynced) { color: #FFFFFF4D !important; }
+        .lyrics-lyrics-contentWrapper>*:not(.lyrics-lyricsContent-active, .lyrics-lyricsContent-highlight, .lyrics-lyricsContent-provider, .lyrics-lyricsContent-description, .lyrics-lyricsContent-unsynced):hover { color: #FFFFFF !important; }
+        .lyrics-lyricsContent-highlight { color: #FFFFFF66; }
+        .lyrics-lyricsContent-unsynced { color: #FFFFFF !important; }
+        .lyrics-lyricsContent-unsynced:hover { color: #FFFFFF !important; }
+        .lyrics-lyricsContent-provider, .lyrics-lyricsContent-description { color: #FFFFFFB6 !important; }
+      ''
+
+      #remove 'now playing view'
+      ''
+        .Root__right-sidebar:has(.NowPlayingView) { width: 0 !important; overflow: hidden !important; }
+        button:has(path[d='M11.196 8 6 5v6l5.196-3z']), button:has(path[d='M11.196 8 6 5v6z']) { display: none; }
+      ''
+    ];
   };
 }
